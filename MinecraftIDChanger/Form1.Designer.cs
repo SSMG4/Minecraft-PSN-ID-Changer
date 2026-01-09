c8a94ebcb420057391d2932e61553a51da29b740
Matthew Gallon <matthew.gallon0912@gmail.com>
2026-01-10 00:35:14 +0100

    Release v1.0.0.0

---
+++ MinecraftIDChanger/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace MinecraftIDChanger
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.cmbRegion = new System.Windows.Forms.ComboBox();
+            this.txtCodeName = new System.Windows.Forms.TextBox();
+            this.txtNewID = new System.Windows.Forms.TextBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.labNewID = new System.Windows.Forms.Label();
+            this.labGeneratedCode = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbRegion
+            // 
+            this.cmbRegion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbRegion.FormattingEnabled = true;
+            this.cmbRegion.Items.AddRange(new object[] {
+            "PCSB00560.psv",
+            "PCSE00491.psv",
+            "PCSG00302.psv"});
+            this.cmbRegion.Location = new System.Drawing.Point(767, 535);
+            this.cmbRegion.Name = "cmbRegion";
+            this.cmbRegion.Size = new System.Drawing.Size(121, 21);
+            this.cmbRegion.TabIndex = 0;
+            this.cmbRegion.Text = "PCSB00560.psv";
+            // 
+            // txtCodeName
+            // 
+            this.txtCodeName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCodeName.Location = new System.Drawing.Point(443, 37);
+            this.txtCodeName.Name = "txtCodeName";
+            this.txtCodeName.Size = new System.Drawing.Size(526, 20);
+            this.txtCodeName.TabIndex = 1;
+            // 
+            // txtNewID
+            // 
+            this.txtNewID.Location = new System.Drawing.Point(13, 37);
+            this.txtNewID.Name = "txtNewID";
+            this.txtNewID.Size = new System.Drawing.Size(425, 20);
+            this.txtNewID.TabIndex = 2;
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbOutput.Location = new System.Drawing.Point(12, 95);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(957, 434);
+            this.rtbOutput.TabIndex = 3;
+            this.rtbOutput.Text = "";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGenerate.Location = new System.Drawing.Point(13, 535);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(100, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate Code";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCopy.Location = new System.Drawing.Point(119, 535);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(894, 535);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // labNewID
+            // 
+            this.labNewID.AutoSize = true;
+            this.labNewID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labNewID.Location = new System.Drawing.Point(8, 9);
+            this.labNewID.Name = "labNewID";
+            this.labNewID.Size = new System.Drawing.Size(86, 25);
+            this.labNewID.TabIndex = 7;
+            this.labNewID.Text = "New ID";
+            // 
+            // labGeneratedCode
+            // 
+            this.labGeneratedCode.AutoSize = true;
+            this.labGeneratedCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labGeneratedCode.Location = new System.Drawing.Point(7, 67);
+            this.labGeneratedCode.Name = "labGeneratedCode";
+            this.labGeneratedCode.Size = new System.Drawing.Size(184, 25);
+            this.labGeneratedCode.TabIndex = 8;
+            this.labGeneratedCode.Text = "Generated Code";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(438, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Code Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 570);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labGeneratedCode);
+            this.Controls.Add(this.labNewID);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.txtNewID);
+            this.Controls.Add(this.txtCodeName);
+            this.Controls.Add(this.cmbRegion);
+            this.Name = "Form1";
+            this.Text = "Minecraft PS Vita PSN ID Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbRegion;
+        private System.Windows.Forms.TextBox txtCodeName;
+        private System.Windows.Forms.TextBox txtNewID;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label labNewID;
+        private System.Windows.Forms.Label labGeneratedCode;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MinecraftIDChanger/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace MinecraftIDChanger
+{
+    public partial class Form1 : Form
+    {
+        // Static memory address for Minecraft PS Vita Username
+        private const uint BASE_ADDRESS = 0x8234628D;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Shown += (s, e) => {
+                cmbRegion.SelectionLength = 0; // Deselect text
+                this.ActiveControl = null;     // Remove focus from the box
+            };
+        }
+
+        // Main logic to generate the Vitacheat code
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string username = txtNewID.Text;
+            string codeName = string.IsNullOrWhiteSpace(txtCodeName.Text) ? "NewID" : txtCodeName.Text;
+
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("Please enter a username.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"_V0 {codeName}");
+
+            byte[] bytes = Encoding.UTF8.GetBytes(username);
+
+            // Process bytes in 4-byte chunks for VitaCheat $0200 (32-bit)
+            for (int i = 0; i < bytes.Length; i += 4)
+            {
+                byte[] chunk = new byte[4] { 0, 0, 0, 0 };
+                int length = Math.Min(4, bytes.Length - i);
+                Array.Copy(bytes, i, chunk, 0, length);
+
+                // Little-endian conversion for the PS Vita's memory
+                Array.Reverse(chunk);
+                string hexValue = BitConverter.ToString(chunk).Replace("-", "");
+
+                uint currentAddress = BASE_ADDRESS + (uint)i;
+                sb.AppendLine($"$0200 {currentAddress:X8} {hexValue}");
+            }
+            rtbOutput.Text = sb.ToString();
+        }
+
+        // Copies the generated text to the Windows Clipboard
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(rtbOutput.Text))
+            {
+                Clipboard.SetText(rtbOutput.Text);
+                MessageBox.Show("Code copied to clipboard!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Opens the File Explorer to save the .psv file
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(rtbOutput.Text))
+            {
+                MessageBox.Show("Please generate a code first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.FileName = cmbRegion.Text;
+                sfd.Filter = "PSV Files|*.psv|All Files|*.*";
+                sfd.Title = "Save Vitacheat File";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.WriteAllText(sfd.FileName, rtbOutput.Text);
+                        MessageBox.Show("File saved successfully!", "Success");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error saving file: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
